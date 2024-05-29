7e11514605301a5999b003f12fa55001edc832e2
m4gn3s1um <madmedmig@gmail.com>
2024-05-29 11:20:51 +0200

    Docker-compose changes

--- Business/db/sql/V3__Game_table.sql
+++ Business/db/sql/V3__Game_table.sql
@@ -1,8 +1,10 @@
-use GameLib;
-    
+USE GameLib;
+
 CREATE TABLE Game (
-    GameId VARCHAR(50) PRIMARY KEY,
-    GameName VARCHAR(50) NOT NULL,
-    ConsoleId VARCHAR(50) FOREIGN KEY REFERENCES Console(ConsoleId),
-    GenreId VARCHAR(50) FOREIGN KEY REFERENCES Genre(GenreId)
-    );
+                      GameId VARCHAR(50) PRIMARY KEY,
+                      GameName VARCHAR(50) NOT NULL,
+                      ConsoleId VARCHAR(50),
+                      GenreId VARCHAR(50),
+                      FOREIGN KEY (ConsoleId) REFERENCES Console(ConsoleId),
+                      FOREIGN KEY (GenreId) REFERENCES Genre(GenreId)
+);
